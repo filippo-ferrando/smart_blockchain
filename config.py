5b56443f284c9b7fa94337fc04d94f8dc6430925
Filippo Ferrando Damillano <filippo.ferrando@itiscuneo.eu>
2022-06-07 16:58:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 FEE = 0.00005
 BPSC_WALLET = './wallets/bpsc_wallet.txt'
 
 def write_amount(wallet, amount):
-    with open(wallet, 'w') as f:
-        f.write(f"amount: {amount}")
+    with open(wallet, 'a') as f:
+        f.write(f"{datetime.now} - amount: {amount}")
 
 def create_wallet(name):
-    with open(f'{name}_wallet.txt', 'w') as f:
+    with open(f'{name}_wallet.txt', 'a') as f:
         f.write(f"amount: 0")
